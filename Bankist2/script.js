8bdf43e4087d9d61a54b5d77ce5fd78d51a9d2a4
melbouab <melbouabdelaoui@gmail.com>
2025-08-25 18:28:24 +0100

    update

--- Bankist2/script.js
+++ Bankist2/script.js
@@ -7,8 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
+const header = document.querySelector('.header');
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,11 +18,8 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
 btnCloseModal.addEventListener('click', closeModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', () => openModal()));
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
@@ -29,3 +27,19 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+// //////////////////////////////
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improving functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+header.append(message.cloneNode(true));
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove('btn');
+    // message.parentElement.removeChild(message);
+  });
+// styles
+message.style.backgroundColor = 'red';
